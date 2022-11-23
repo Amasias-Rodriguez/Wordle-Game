3f21fe893b3f9bc5d38572dc957a62af71bd561d
Rod-aDev <100006082+Rod-aDev@users.noreply.github.com>
2022-11-22 23:00:44 -0600

    Update Wordle.js

--- Wordle.js
+++ Wordle.js
@@ -14,156 +14,159 @@ const options = {
 };
 
 fetch('https://1000-most-common-words.p.rapidapi.com/words/spanish?words_limit=1', options)
-.then(result => console.log(result.json()))
-
-
-
-
-
-let word = 'texto';
-let wordArray = word.toUpperCase().split('');
-console.log(wordArray)
-
-let actualRow = document.querySelector('.row')
-
-
-
-drawSquares(actualRow);
-listenInput(actualRow)
-addFocus(actualRow)
-
-function listenInput(actualRow){
-    let squares = actualRow.querySelectorAll('.square');
-    squares = [...squares];
-
-
-    let userInput = []
-
-    squares.forEach(element => {
-    element.addEventListener('input', event=>{
-        //Si no se ha borrado
-        if(event.inputType !== 'deleteContentBackward'){
-            //Recoger el ingreso del usuario
-        userInput.push(event.target.value.toUpperCase())
-        console.log(userInput)
-        if(event.target.nextElementSibling){
-            event.target.nextElementSibling.focus();
-        }else{
-            //Crear arreglo con las letras
-            let squaresFilled = actualRow.querySelectorAll('.square');
-            squaresFilled = [...squaresFilled]
-            let lastFiveSquaresFilled = squaresFilled.slice(-5);
-            let finalUserInput = [];
-            lastFiveSquaresFilled.forEach(element => {
-                finalUserInput.push(element.value.toUpperCase)
-            });
-
-
-            // Cambiar estilos si existe la letra pero no esta en la posicion correcta 
-            let existIndexArray = existLetter(wordArray, finalUserInput)
-            console.log(existIndexArray)
-             existIndexArray.forEach(element =>{
-                squares[element].classList.add('gold');
-            });
-
-            // Comparar arreglos para cambiar estilos
-           let rightIndex =  compareArrays(wordArray, finalUserInput)
-           console.log(rightIndex)
-           rightIndex.forEach(element => {
-            squares[element].classList.add('green');
-           })
-
-
-        //    Si los arreglos son iguales
-
-        if(rightIndex.length == wordArray.length){
-            showResult('You Win!')
-
-            return;
-        }
-
-        // Crear una nueva linea
-        let actualRow = createRow()
-        if(!actualRow){
-            return
-        }
-        drawSquares(actualRow)
-        listenInput(actualRow)
-        addFocus(actualRow)
-
-        }
-        }else{
-            userInput.pop();
-        }
-        console.log(userInput)
-    });
+.then(result => result.json())
+.finally(()=>{
+    let loadingElement = document.querySelector('.loading')
+    loadingElement.style.display = 'none';
 })
-}
+.then(data => {
+
+    let word = data[0];
+    let wordArray = word.toUpperCase().split('');
+    console.log(wordArray)
+    var actualRow = document.querySelector('.row')
 
 
 
-//Funciones
+    drawSquares(actualRow);
+    listenInput(actualRow)
+    addFocus(actualRow)
 
-function compareArrays(array1, array2){
-    let iqualsIndex = []
-    array1.forEach((element, index)=>{
-        if(element == array2[index]){
-            console.log('En la posicion ${index} si son iguales');
-            iqualsIndex.push(index);
-        }else{
-            console.log('En la posicion ${index} NO son iguales');
-        }
-    });
-    return iqualsIndex;
-}
+    function listenInput(actualRow){
+        let squares = actualRow.querySelectorAll('.square');
+        squares = [...squares];
 
-function existLetter(array1, array2){
-    let existIndexArray = [];
-    array2.forEach((element, index)=>{
-        if(array1.includes(element)){
-            existWordsArray.push(index)
-        }
-    });
-    return existIndexArray;
-}
 
-function createRow(){
-    rowId++
-    if (!rowId <= 5){
-        let newRow = document.createElement('div');
-        newRow.classList.add('row');
-        newRow.setAttribute('id', 'rowId')
-        mainContainer.appendChild(newRow)
-        return newRow;  
-    }else{
-        showResult('Try Again')
+        let userInput = []
+
+        squares.forEach(element => {
+        element.addEventListener('input', event=>{
+            //Si no se ha borrado
+            if(event.inputType !== 'deleteContentBackward'){
+                //Recoger el ingreso del usuario
+            userInput.push(event.target.value.toUpperCase())
+            console.log(userInput)
+            if(event.target.nextElementSibling){
+                event.target.nextElementSibling.focus();
+            }else{
+                //Crear arreglo con las letras
+                let squaresFilled = actualRow.querySelectorAll('.square');
+                squaresFilled = [...squaresFilled]
+                let lastFiveSquaresFilled = squaresFilled.slice(-word.length);
+                let finalUserInput = [];
+                lastFiveSquaresFilled.forEach(element => {
+                    finalUserInput.push(element.value.toUpperCase)
+                });
+
+
+                // Cambiar estilos si existe la letra pero no esta en la posicion correcta 
+                let existIndexArray = existLetter(wordArray, finalUserInput)
+                console.log(existIndexArray)
+                existIndexArray.forEach(element =>{
+                    squares[element].classList.add('gold');
+                });
+
+                // Comparar arreglos para cambiar estilos
+            let rightIndex =  compareArrays(wordArray, finalUserInput)
+            console.log(rightIndex)
+            rightIndex.forEach(element => {
+                squares[element].classList.add('green');
+            })
+
+
+            //    Si los arreglos son iguales
+
+            if(rightIndex.length == wordArray.length){
+                showResult('You Win!')
+
+                return;
+            }
+
+            // Crear una nueva linea
+            let actualRow = createRow()
+            if(!actualRow){
+                return
+            }
+            drawSquares(actualRow)
+            listenInput(actualRow)
+            addFocus(actualRow)
+
+            }
+            }else{
+                userInput.pop();
+            }
+            console.log(userInput)
+        });
+    })
     }
-    
-}
 
-function drawSquares(actualRow){
-    wordArray.forEach((item, index) => {
-        if(index === 0){
-            actualRow.innerHTML += `<input type="text" maxlength="1" class="square focus">`
+
+
+    //Funciones
+
+    function compareArrays(array1, array2){
+        let iqualsIndex = []
+        array1.forEach((element, index)=>{
+            if(element == array2[index]){
+                console.log('En la posicion ${index} si son iguales');
+                iqualsIndex.push(index);
+            }else{
+                console.log('En la posicion ${index} NO son iguales');
+            }
+        });
+        return iqualsIndex;
+    }
+
+    function existLetter(array1, array2){
+        let existIndexArray = [];
+        array2.forEach((element, index)=>{
+            if(array1.includes(element)){
+                existWordsArray.push(index)
+            }
+        });
+        return existIndexArray;
+    }
+
+    function createRow(){
+        rowId++
+        if (!rowId <= 5){
+            let newRow = document.createElement('div');
+            newRow.classList.add('row');
+            newRow.setAttribute('id', 'rowId')
+            mainContainer.appendChild(newRow)
+            return newRow;  
         }else{
-            actualRow.innerHTML += `<input type="text" maxlength="1" class="square">`
+            showResult('Try Again')
         }
-    });
-}
+        
+    }
 
-function addFocus(actualRow){
-    let focusElement = actualRow.querySelector('.focus')
-    console.log(focusElement)
-    focusElement.focus();
-}
+    function drawSquares(actualRow){
+        wordArray.forEach((item, index) => {
+            if(index === 0){
+                actualRow.innerHTML += `<input type="text" maxlength="1" class="square focus">`
+            }else{
+                actualRow.innerHTML += `<input type="text" maxlength="1" class="square">`
+            }
+        });
+    }
 
-function showResult(textMsg){
-    resultElement.innerHTML = `
-    <p>${textMsg}</p>
-    <button class="button">Restart</button>`
+    function addFocus(actualRow){
+        let focusElement = actualRow.querySelector('.focus')
+        console.log(focusElement)
+        focusElement.focus();
+    }
 
-    let resetBtn = document.querySelector('.button')
-    resetBtn.addEventListener('click', ()=>{
-        location.reload();
-    });
-}
+    function showResult(textMsg){
+        resultElement.innerHTML = `
+        <p>${textMsg}</p>
+        <button class="button">Restart</button>`
+
+        let resetBtn = document.querySelector('.button')
+        resetBtn.addEventListener('click', ()=>{
+            location.reload();
+        });
+    }
+    })
+
+
